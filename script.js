07e200ad60aa6096aec21cd97911e3f7c617dfd2
CodeBlueYellow <Seanpculkin@yahoo.com>
2026-08-31 16:19:27 -0400

    Added Javascript to allow my email to be copied on click event.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const image = document.getElementById('toggleImage');
 const images = ['images/lake.jpg', 'images/mount.jpg'];
+const email = document.getElementById('email');
 let index = 0;
 
 image.addEventListener('click', () => {
     index = (index + 1) % images.length;
     image.src = images[index];}
 );
+
+email.addEventListener("click", function(){
+    navigator.clipboard.writeText(email.text);
+});
+
